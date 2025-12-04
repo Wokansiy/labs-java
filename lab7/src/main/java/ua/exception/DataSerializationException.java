28e6fd15061b1cb44ffe20e11089d9480cab69e3
Wokansiy <82878803+Wokansiy@users.noreply.github.com>
2025-12-04 22:17:35 +0200

    Delete lab7 directory

--- lab7/src/main/java/ua/exception/DataSerializationException.java
+++
@@ -1,9 +0,0 @@
-package ua.exception;
-public class DataSerializationException extends RuntimeException{
-    public DataSerializationException(String message){
-        super(message);
-    }
-    public DataSerializationException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
--- lab7/src/main/java/ua/sports/CoachRole.java
+++
@@ -1,4 +0,0 @@
-package ua.sports;
-public enum CoachRole{
-    HEAD_COACH,ASSISTANT_COACH,FITNESS_COACH,GOALKEEPING_COACH
-}
