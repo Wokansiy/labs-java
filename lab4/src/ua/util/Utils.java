edc7eb301700d87a03d5d876284d45ab43d94eb5
Wokansiy <82878803+Wokansiy@users.noreply.github.com>
2025-12-04 22:14:34 +0200

    Delete lab4/src/ua directory

--- lab4/src/ua/util/Utils.java
+++
@@ -1,8 +0,0 @@
-package ua.util;
-import java.time.*;import java.time.format.*;
-public final class Utils{
-    private static final DateTimeFormatter F=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    private Utils(){}
-    public static String formatFullName(String f,String l){ return f+" "+l; }
-    public static String formatDateTime(LocalDateTime dt){ return dt.format(F); }
-}
